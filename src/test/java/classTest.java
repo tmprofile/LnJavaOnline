7b9d756dafa7db374d7f1cea9a4f87b2d39eec97
Tmprofile Tmprofile <tmprofile@yandex.ru>
2021-11-03 18:03:54 +0300

    testVariablesAndTypes method has been updated + comments in testHelloWorld

--- src/test/java/classTest.java
+++ src/test/java/classTest.java
@@ -3,13 +3,21 @@ import org.testng.annotations.Test;
 public class classTest {
     @Test
     public void testHelloWorld() {
+        // Print "Hello, World!" to the console.
         System.out.println("Hello, World!");
     }
 
     @Test
     public void testVariablesAndTypes() {
+        //Create all of the primitives (except long and double) with different values.
+        //Concatenate them into a string and print it to the screen so it will print: H3110 w0r1d 2.0 true
         byte zero = 0;
-        String output = "W" + zero + "w";
+        short a = 3;
+        int b = 1;
+        char d = ' ';
+        float e = 2.0f;
+        boolean f = true;
+        String output = "H" + a + b + b + zero + d + "w" + zero + "r" + b + "d" + d + e + d + f;
         System.out.println(output);
     }
 
