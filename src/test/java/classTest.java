601ac5a4d9238216f1985acdc5732566d49a2dd8
Tmprofile Tmprofile <tmprofile@yandex.ru>
2021-11-03 18:16:50 +0300

    testConditionals method has been modified

--- src/test/java/classTest.java
+++ src/test/java/classTest.java
@@ -21,13 +21,12 @@ public class classTest {
         System.out.println(output);
     }
 
-
-
     @Test
     public void testConditionals() {
-        String a = new String("Wow");
-        String b = "Wow";
-        String c = a;
+        // Change the variables in the first section, so that each if statement resolves as true.
+        String a = "Wow";
+        String b = a;
+        String c = b + "!";
         String d = c;
 
         boolean b1 = a == b;
