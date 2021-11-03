32dad349f22cd238b2a3a7ac369e217fbe604164
Tmprofile Tmprofile <tmprofile@yandex.ru>
2021-11-03 16:42:18 +0300

    testHelloWorld method added

---
+++ src/test/java/classTest.java
@@ -0,0 +1,10 @@
+import org.testng.annotations.Test;
+
+public class classTest {
+    @Test
+    public void testHelloWorld() {
+        System.out.println("This will be printed");
+    }
+
+    
+}
