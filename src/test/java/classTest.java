3cb872a01c4ba0812c11a64633226364b368e5d9
Tmprofile Tmprofile <tmprofile@yandex.ru>
2021-11-03 16:53:07 +0300

    testConditionals method added

--- src/test/java/classTest.java
+++ src/test/java/classTest.java
@@ -12,4 +12,20 @@ public class classTest {
         String output = "W" + zero + "w";
         System.out.println(output);
     }
+
+    @Test
+    public void testConditionals() {
+        String a = new String("Wow");
+        String b = "Wow";
+        String c = a;
+        String d = c;
+
+        boolean b1 = a == b;
+        boolean b2 = d.equals(b + "!");
+        boolean b3 = !c.equals(a);
+
+        if (b1 && b2 && b3) {
+            System.out.println("Success!");
+        }
+    }
 }
