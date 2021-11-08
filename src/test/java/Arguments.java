f0a93026d9afed269b665fd2b448fd8afba9ae47
Tmprofile Tmprofile <tmprofile@yandex.ru>
2021-11-08 14:40:39 +0300

    The exercise program basen on new Arguments class created

---
+++ src/test/java/Arguments.java
@@ -0,0 +1,8 @@
+public class Arguments {
+    // Create a program that prints the arguments of our program. Write one argument per line.
+    public static void main(String[] args) {
+        for (int i = 0; i < args.length; i++) {
+            System.out.println(args[i]);
+        }
+    }
+}
